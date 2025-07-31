82616b91c5f38e2708124d698b8a891108762cc8
Anson Saju George <ansonsaju@karunya.edu.in>
2025-07-31 11:38:15 +0530

    Update

--- _02_Basic_DSA/run_rename.py
+++ _02_Basic_DSA/run_rename.py
@@ -1,4 +1,14 @@
 import os
 
-dir = os.listdir("./_02_Basic_DSA")
-print(dir)
+dir1 = os.listdir("./_01_Fundamentals")
+dir2 = os.listdir("./_02_Basic_DSA")
+
+dir1 = [_ for _ in dir1 if _.endswith(".java")]
+dir2 = [_ for _ in dir2 if _.endswith(".java")]
+
+for _ in dir1:
+    s  = ''
+
+
+print(dir1[0:2])
+print(dir2[0:2])
